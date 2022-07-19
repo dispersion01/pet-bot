dc3d9a58d5d835b6c6506d6aadd64c45ade7be4d
dispersion01 <anyta1901@gmail.com>
2022-07-19 20:42:15 +0300

    Initial commit

---
+++ src/main/java/com/skypro/petbot/PetBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.petbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/petbot/PetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.petbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
