41d0c8a0c6111a60bb821f64edcf0526f1536106
AnnaLihanova <anja.lihanova@gmail.com>
2022-08-08 20:30:27 +0300

    Создание таблицы

--- src/main/resources/liquibase/scripts/pet-bot.sql
+++ src/main/resources/liquibase/scripts/pet-bot.sql
@@ -1,3 +1,10 @@
 -- liquibase formatted sql
 
--- changeset ...
+-- changeset annaLihanova:1
+CREATE TABLE IF NOT EXISTS notification_task
+(
+    id                serial    NOT NULL PRIMARY KEY,
+    chat_id           bigint    NOT NULL,
+    date_time timestamp NOT NULL,
+    message           text   NOT NULL
+);
