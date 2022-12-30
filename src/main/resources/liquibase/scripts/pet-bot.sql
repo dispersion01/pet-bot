60d830c9cdf7886fd666a15217b6852d5b6a612f
dispersion01 <anyta1901@gmail.com>
2022-12-31 00:53:48 +0400

    Исправлены еще некоторые замечания от 31 декабря

--- src/main/resources/liquibase/scripts/pet-bot.sql
+++ src/main/resources/liquibase/scripts/pet-bot.sql
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS pet
 (
     id                bigint   NOT NULL PRIMARY KEY,
     name              VARCHAR   NOT NULL,
-    age               INTEGER   NOT NULL,
+    age               BIGINT   NOT NULL,
     date_adoption     timestamp NOT NULL
     );
 create table if not exists report
 (
-    id      integer not null primary key,
+    chat_id      integer not null primary key,
     photo   bytea   not null,
     message varchar not null
 );
